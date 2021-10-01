759866784f1a35677d3b36e61c7676a55825440a
Midnight <arreiskurai@yahoo.com>
2021-10-01 12:41:00 -0700

    Finished adding the ability to add new songs.  Also able to view song details individually independent from the list of songs.

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 # Create your views here.
 class SongList(APIView):
 
@@ -13,9 +14,30 @@ class SongList(APIView):
         serializer = SongSerializer(song, many=True)
         return Response(serializer.data)
 
+class SongNew(APIView):
+
     def post(self, request):
         serializer = SongSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+    
+    def get_object(self, pk):
+        song = Song.objects.get(pk=pk)
+        return song
+    
+    def get(self, request, pk):
+        song = Song.objects.get(pk=pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
